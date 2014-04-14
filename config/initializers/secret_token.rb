0c349c35782e9c27b1eb8941924264db1f6a18bf
Ravi Teja (Darien) <teja.darien@gmail.com>
2014-04-14 16:48:48 +0530

    Postgre initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+App::Application.config.secret_key_base = '0800d19da3721d626dc82c285ef4c31ec50efe7486c69a2721695e9533d0f5da8f24f7284dcd3faf83e555b8dd1715f45b6db68839b9fbc7ed5280717701fa2a'
